0e90ffaac06d27c4ee8fa6b78b740a8d32e08575
ParkHyeonmin <824hm@naver.com>
2024-04-08 17:29:56 +0900

    2024-04-08 / 박현민 / 강의DTO 수정

--- Q_Pool/src/main/java/com/spring/qpool/biz/lecture/LectureDTO.java
+++ Q_Pool/src/main/java/com/spring/qpool/biz/lecture/LectureDTO.java
@@ -10,7 +10,7 @@ public class LectureDTO {
 	private int lectureIdx;			// 강의 번호
 	private String lectureTitle;	// 강의 제목
 	private String lectureInfo;		// 강의 설명
-	private String teacher;			// 강사
+	private String instructor;		// 강사
 	private int price;				// 가격
 	private Date regDate;			// 시작일
 	private Date endDate;			// 종료일
